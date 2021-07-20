48f98e4c650521266d3e110588a337732879f22c
ismaail <contact@ismaail.com>
2021-07-20 03:59:12 +0100

    Add Board View

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import('@views/About.vue')
-    }
+    },
+    {
+        path: '/board',
+        name: 'Board',
+        component: () => import('@views/Board.vue'),
+    },
 ];
 
 const router = createRouter({
