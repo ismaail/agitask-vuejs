97a80c62f9e0e7ead42a375a13fcb8c6304062d6
ismaail <contact@ismaail.com>
2021-07-20 03:57:27 +0100

    Add namespaced Board Store

---
+++ src/store/Board/getters.js
@@ -0,0 +1,12 @@
+export default {
+    /**
+     * Return all Lists
+     *
+     * @param state
+     *
+     * @return {[{id: number, title: string}]}
+     */
+    all(state) {
+        return state.lists;
+    },
+}
