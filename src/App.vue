bc46cde1367dde95dc1613c43fd861c8a99b1d8b
ismaail <contact@ismaail.com>
2021-07-21 04:19:49 +0100

    Update App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-	<div id="nav">
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link> |
-		<router-link to="/board">Board</router-link>
-	</div>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-gray-dark">
+		<div class="container-fluid">
+			<router-link to="/" class="navbar-brand">AgiTask</router-link>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarText">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<!--<a class="nav-link active" aria-current="page" href="#">Home</a>-->
+						<router-link to="/" class="nav-link">Home</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link to="/board" class="nav-link">Board</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link to="/about" class="nav-link">About</router-link>
+					</li>
+				</ul>
+				<img src="./assets/images/avatar.png" alt="" class="navbar-avatar">
+			</div>
+		</div>
+	</nav>
 	<router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
