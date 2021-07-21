cb6d3281acc01e423ce95b62f5543398ba7d0bc4
ismaail <contact@ismaail.com>
2021-07-21 04:19:31 +0100

    customize router-link 'active' class

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,8 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    linkActiveClass: 'active',
 });
 
 export default router
