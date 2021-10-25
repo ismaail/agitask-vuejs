c52228d935e349a77a3168523a0f363e13d68b3f
ismaail <contact@ismaail.com>
2021-10-25 04:17:00 +0100

    Add fetch() action method to Board Store

--- src/store/Board/actions.js
+++ src/store/Board/actions.js
@@ -1,2 +1,31 @@
+import axios from "axios";
+import boardsQuery from "~/graphql/queries/boards";
+
 export default {
+	/**
+	 * Fetch all Boards.
+	 *
+	 * @param commit
+	 * @return {Promise<void>}
+	 */
+	async fetch({ commit }) {
+		const accessToken = localStorage.getItem('accessToken');
+
+		const response = await axios({
+			url: '//api.agitasks.localhost/graphql',
+			method: 'post',
+			headers: {
+				'content-type': 'application/json',
+				Authorization: `bearer ${accessToken}`
+			},
+			data: {
+				query: boardsQuery,
+				variables: {
+					limit: 10,
+				}
+			},
+		});
+
+		commit('setBoards', response.data.data.boards);
+	}
 }
